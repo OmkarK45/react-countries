164bfd9e487af352281e745ec145f7bb2971b33d
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-29 19:28:19 +0530

    Created Components using SFC

---
+++ src/components/Countries.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Coutries = (props) => {
+    return ( 
+        <h1>SHowing list of all countries</h1>
+     );
+}
+ 
+export default Coutries;
---
+++ src/components/Country.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Country = (props) => {
+    return ( <h1>Im a country</h1> );
+}
+ 
+export default Country;
