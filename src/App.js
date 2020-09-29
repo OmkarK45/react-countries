7b814a14b13c986c3a0db6faf34cce7c38d7d3e1
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-29 22:21:12 +0530

    Added Nav import

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Country from "./components/Country";
-import Countries from "./components/Countries";
+import Countries from "./components/allCountries";
+import Nav from './components/Nav'
+
 const App = () => {
   return (
     <React.Fragment>
       <Router>
+        <Nav/>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/all" exact component={Countries} />
