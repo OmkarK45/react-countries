2f95b57a02f5ece9396f11f8f2bd4408c65c6558
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-30 13:58:25 +0530

    Added img and style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-
+import img from '../img.png'
+import { Link } from "react-router-dom";
 const Home = (props) => {
   return (
     <React.Fragment>
-      <div className='home-wrapper'>
+      <div className="home-wrapper">
         <div className="left">
-          test
+          <div className="text-holder">
+            <h2 className="home-hero-upper">Explore</h2>
+            <h2 className="home-hero-bottom">Countries</h2>
+            <Link to="/all">
+              <button className="btn btn-nav">SHOW ALL</button>
+            </Link>
+          </div>
         </div>
         <div className="right">
-          test
+          <img src={img} className='hero-image' alt=""/>
         </div>
       </div>
     </React.Fragment>
