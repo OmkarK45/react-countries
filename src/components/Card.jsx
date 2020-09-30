71accab10f82ed0c5e170388b11c0257d163319e
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-30 16:10:07 +0530

    Added Currency rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,40 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 const Card = ({ country }) => {
   // console.log(data);
   return (
-      <div className="card">
-          <div className="img-wrapper">
-              <img src={country.flag} alt=""/>
-          </div>
-          <div className="main">
-              {/* 1. Country Title
+    <div className="card">
+      <div className="img-wrapper">
+        <img src={country.flag} alt="" />
+      </div>
+      <div className="main">
+        {/* 1. Country Title
               2. Country Code
               3. Country Currency */}
-              
-              <Link className='name' key={country.alpha2Code} to={{
-              pathname: `all/${country.alpha2Code}`,
-              state: {...{country}}
-            }}>{country.name}</Link>
-            <p className='capital'>{country.capital}</p>
-        <span className='region'><i className="far fa-globe-asia"></i> {country.region}</span>
-          </div>
 
+        <Link
+          className="name"
+          key={country.alpha2Code}
+          to={{
+            pathname: `all/${country.alpha2Code}`,
+            state: { ...{ country } },
+          }}
+        >
+          {country.name}
+        </Link>
+          <p className="capital">{country.capital}</p>
+        <div className='tag-wrapper'>
+          <span className="region">
+            <i className="far fa-globe-asia"></i> {country.region}
+          </span>
+          {country.currencies.map((currency) => (
+            <span key={currency.code} className="currency">
+              <i className="far fa-money-bill-wave-alt"></i> {currency.code}
+            </span>
+          ))}
+        </div>
       </div>
+    </div>
   );
 };
 
