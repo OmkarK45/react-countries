93eabb066c66e69700bc404a024ad2459f2a835a
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-30 16:41:07 +0530

    Fixed the bug when all cards are stretched.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,38 +3,41 @@ import { Link } from "react-router-dom";
 const Card = ({ country }) => {
   // console.log(data);
   return (
-    <div className="card">
-      <div className="img-wrapper">
-        <img src={country.flag} alt="" />
-      </div>
-      <div className="main">
-        {/* 1. Country Title
-              2. Country Code
-              3. Country Currency */}
+    <React.Fragment>
+      <div className="card">
+        <div className="img-wrapper">
+          <img src={country.flag} alt="" />
+        </div>
+        <div className="main">
+            
+          <div className="wrapper">
+            <Link
+              className="name"
+              key={country.alpha2Code}
+              to={{
+                pathname: `all/${country.alpha2Code}`,
+                state: { ...{ country } },
+              }}
+            >
+              {country.name}
+            </Link>
 
-        <Link
-          className="name"
-          key={country.alpha2Code}
-          to={{
-            pathname: `all/${country.alpha2Code}`,
-            state: { ...{ country } },
-          }}
-        >
-          {country.name}
-        </Link>
-          <p className="capital">{country.capital}</p>
-        <div className='tag-wrapper'>
-          <span className="region">
-            <i className="far fa-globe-asia"></i> {country.region}
-          </span>
-          {country.currencies.map((currency) => (
-            <span key={currency.code} className="currency">
-              <i className="far fa-money-bill-wave-alt"></i> {currency.code}
+            <p className="capital">{country.capital}</p>
+          </div>
+
+          <div className="tag-wrapper">
+            <span className="region">
+              <i className="far fa-globe-asia"></i> {country.region}
             </span>
-          ))}
+            {country.currencies.map((currency) => (
+              <span key={currency.code} className="currency">
+                <i className="far fa-money-bill-wave-alt"></i> {currency.code}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
